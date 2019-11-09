ed1b1667c8681dae229dc814fe5b8b49eebbdb18
aramkoukia <aramkoukia@gmail.com>
2019-11-08 17:52:28 -0800

    works now

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -1,6 +1,35 @@
-import React from "react";
-import LandingPage from "./LandingPage/LandingPage";
+import React from 'react';
+import { Link } from 'gatsby';
+import LandingPage from './LandingPage/LandingPage';
 
-export default () => (
-  <LandingPage/>
+export default ({ data }) => (
+  <div>
+  <ul>
+    {data.allStrapiCategory.edges.map(document => (
+      <li key={document.node.id}>
+        <h2>
+          <Link to={`/${document.node.id}`}>
+            {document.node.Name}
+
+          </Link>
+        </h2>
+      </li>
+    ))}
+  </ul>
+  <LandingPage />
+</div>
 );
+
+
+export const pageQuery = graphql`
+  query SectionCategoriesQuery {
+    allStrapiCategory {
+      edges {
+        node {
+          id
+          Name
+        }
+      }
+    }
+  }
+`;
