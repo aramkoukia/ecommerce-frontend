a9aff25c22be6a1e97ff0748ece3134d3b80f2b9
aramkoukia <aramkoukia@gmail.com>
2019-11-13 19:45:00 -0800

    add strapi products

--- src/templates/product-by-category.jsx
+++ src/templates/product-by-category.jsx
@@ -11,18 +11,14 @@ export default ProductsByCategoryTemplate;
 export const query = graphql`
   query ProductsByCategoryTemplate($id: String!) {
     strapiProducts(id: {eq: $id}) {
-      title
-      content
-      image {
-          childImageSharp {
-            fixed(width: 200, height: 125) {
-              ...GatsbyImageSharpFixed
-            }
-          }
+      edges {
+        node {
+          id
+          ProductCode
+          ProductName
+          ShortDescription
+          LongDescription
         }
-      author {
-        id
-        username
       }
     }
   }
