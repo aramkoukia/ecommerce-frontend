38f790092e140b2c71d3d99643663df5a261700c
aramkoukia <aramkoukia@gmail.com>
2019-11-13 18:57:42 -0800

    lint

---
+++ src/templates/product-by-category.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+// import Img from 'gatsby-image';
+
+const ProductsByCategoryTemplate = ({ data }) => (
+  <h1>Products By Category</h1>
+);
+
+export default ProductsByCategoryTemplate
+
+export const query = graphql`
+  query ProductsByCategoryTemplate($id: String!) {
+    strapiProducts(id: {eq: $id}) {
+      title
+      content
+      image {
+          childImageSharp {
+            fixed(width: 200, height: 125) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      author {
+        id
+        username
+      }
+    }
+  }
+`;
