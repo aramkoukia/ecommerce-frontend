9f43dc5515f263bbb0af50edb966b29e735cada4
aramkoukia <aramkoukia@gmail.com>
2019-11-16 13:57:37 -0800

    still trying to add image

--- src/pages/LandingPage/Sections/CategoriesSection.jsx
+++ src/pages/LandingPage/Sections/CategoriesSection.jsx
@@ -12,6 +12,7 @@ const CategoriesSection = ({ data }) => (
             <h2>
               <Link to={`/${document.node.id}`}>{document.node.Name}</Link>
               <img src={document.node.Thumbnail.url} alt={document.node.Name} />
+              <Img fixed={document.node.Thumbnail} />
             </h2>
             <p>
               {/* <Img fixed={document.node.Tumbnail} /> */}
