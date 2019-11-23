ce1b0e7cc24cff5bfe911353f1ef406f5954a8e9
aramkoukia <aramkoukia@gmail.com>
2019-11-23 15:54:27 -0800

    added menu to category page

--- src/templates/product-by-category.jsx
+++ src/templates/product-by-category.jsx
@@ -2,31 +2,54 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
-const CategoryTemplate = ({ data }) => (
+import Header from '../components/Header/Header';
+import HeaderLinks from '../components/Header/HeaderLinks';
+import Parallax from '../components/Parallax/Parallax';
+
+const CategoryTemplate = ({ data, classes }) => (
   <div>
-    <h1>{data.strapiCategory.Name}</h1>
-    <ul>
-      {data && data.strapiCategory.Products.map(product => (
-        <li key={product.ProductCode}>
-          <h2>
-            {product.ProductCode}
-            <Link to={`/${product.ProductName}`}>
-              {product.ProductName}
-            </Link>
-            {product.ShortDescription}
-          </h2>
-          <p>
+    <Header
+      color="primary"
+      brand="LED Lights And Parts"
+      rightLinks={<HeaderLinks />}
+      fixed
+      changeColorOnScroll={{
+        height: 400,
+        color: 'white',
+      }}
+    />
+    {/* <Parallax filter image={require('../../assets/img/page-bg.jpg')}>
+      <div />
+    </Parallax> */}
+    <div>
+      <div>
+        <br />
+        <br />
+        <h1>{data.strapiCategory.Name}</h1>
+        <ul>
+          {data && data.strapiCategory.Products.map(product => (
+            <li key={product.ProductCode}>
+              <h2>
+                {product.ProductCode}
+                <Link to={`/${product.ProductName}`}>
+                  {product.ProductName}
+                </Link>
+                {product.ShortDescription}
+              </h2>
+              <p>
             url:
-            {' '}
-            {product.Image[0].url}
-          </p>
-          <p>
-            <img src={product.Image[0].url} alt={product.ProductName} />
-            <Img fixed={product.Image[0]} />
-          </p>
-        </li>
-      ))}
-    </ul>
+                {' '}
+                {product.Image[0].url}
+              </p>
+              <p>
+                <img src={product.Image[0].url} alt={product.ProductName} />
+                <Img fixed={product.Image[0]} />
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
 
   </div>
 );
