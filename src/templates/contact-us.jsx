fcad98c4e10dca2b6ad41dc5f5b97d0d88448e79
aramkoukia <aramkoukia@gmail.com>
2019-12-01 20:56:03 -0800

    contact template

---
+++ src/templates/contact-us.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const ContactUsTemplate = ({ data, classes }) => (
+  <Layout>
+    <h1>Contact Us</h1>
+  </Layout>
+);
+
+export default ContactUsTemplate;
