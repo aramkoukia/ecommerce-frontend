8ffd19a01ad98ebacf47c93c87635674fdaa6e6c
Aram Koukia <aramkoukia@gmail.com>
2020-05-18 12:37:57 -0700

    renamed landing to home

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
-// nodejs library that concatenates classes
 import classNames from 'classnames';
-// nodejs library to set properties for components
 import PropTypes from 'prop-types';
-// @material-ui/core components
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -12,9 +9,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import Hidden from '@material-ui/core/Hidden';
 import Drawer from '@material-ui/core/Drawer';
-// @material-ui/icons
 import Menu from '@material-ui/icons/Menu';
-// core components
 import styles from '../../assets/jss/nextjs-material-kit/components/headerStyle';
 
 const useStyles = makeStyles(styles);
@@ -64,7 +59,7 @@ export default function Header(props) {
     [classes.fixed]: fixed,
   });
   const brandComponent = (
-    <Link href="/components" as="/components">
+    <Link href="/home" as="/home">
       <Button className={classes.title}>{brand}</Button>
     </Link>
   );
